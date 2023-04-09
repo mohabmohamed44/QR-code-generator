5c1bc464303b07e236075052cbd91028d5fcb907
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2023-04-10 00:51:53 +0200

    final commit

--- js/script.js
+++ js/script.js
@@ -9,4 +9,29 @@ function generateQRCode() {
         correctLevel: QRCode.CorrectLevel.H
     });
     qrcode.makeCode(text);
+
+    // Create a canvas element with the QR code image
+    var canvas = document.getElementById("qrcode").getElementsByTagName("canvas")[0];
+
+    // Set the canvas background color to transparent
+    canvas.style.backgroundColor = "transparent";
+
+    // Create a new image element with the QR code image as its source
+    var img = new Image();
+    img.src = canvas.toDataURL("image/png");
+
+    // Show the download button and set its href attribute to the image source
+    var downloadBtn = document.getElementById("download-btn");
+    downloadBtn.style.display = "inline-block";
+    downloadBtn.onclick = function () {
+        // Create a new temporary link element and set its href and download attributes
+        var link = document.createElement("a");
+        link.href = img.src;
+        link.download = "qrcode.png";
+
+        // Append the link to the document and click it to download the image
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
 }
